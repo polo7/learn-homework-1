5f58f1b7587e6c162a506f3d31e24d7f6584ef21
Roman Lesechko <polo-ru@yandex.ru>
2020-02-18 23:44:20 +0300

    Простой перебор без доп.проверок.

--- if1.py
+++ if1.py
@@ -19,7 +19,25 @@ def main():
     Эта функция вызывается автоматически при запуске скрипта в консоли
     В ней надо заменить pass на ваш код
     """
-    pass
+    def get_occupation(age):
+      if age >= 3 and age <= 7:
+        occupation = 'должны ходить в детский сад'
+      elif age > 7 and age <= 18:
+        occupation = 'должны учиться в школе'
+      elif age > 18 and age <= 23:
+        occupation = 'должны учиться в ВУЗе'
+      elif age > 23 and age <= 65:
+        occupation = 'должны работать'
+      else:
+        occupation = 'ничем не занимаетесь'
+      return occupation
+
+    user_age = int(input('Введите ваш возраст (целое число больше ноля): '))
+    occupation = get_occupation(user_age)
+    print(f'Судя по возрасту, вы {occupation}.')
+
+
+    
 
 if __name__ == "__main__":
     main()
